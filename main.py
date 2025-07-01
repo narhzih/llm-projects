cec734e0cd463ebd85499c42d4679854b3f61ea0
narhzih <omosekejiolawale@gmail.com>
2025-07-01 11:58:01 +0100

    Created a simple chatbot for terminal

--- main.py
+++ main.py
@@ -1,50 +1,46 @@
-from langchain.llms import OpenAI
-from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain, SequentialChain
+from langchain.prompts import MessagesPlaceholder, HumanMessagePromptTemplate, ChatPromptTemplate
+from langchain.memory import ConversationBufferMemory, FileChatMessageHistory, ConversationSummaryMemory
+from langchain_openai import ChatOpenAI
+from langchain.chains import LLMChain
 from dotenv import load_dotenv
-import argparse
 
 load_dotenv()
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--task", default="return a list of numbers")
-parser.add_argument("--language", default="python")
-args = parser.parse_args()
+chat = ChatOpenAI()
+
+# memory = ConversationBufferMemory(
+#     chat_memory=FileChatMessageHistory("messages.json"),
+#     memory_key="messages", 
+#     return_messages=True,
+
+# )
 
 
-llm = OpenAI()
-
-code_prompt = PromptTemplate(
-    template="Write a very short {language} function that will {task}",
-    input_variables=["language", "task"]
+memory = ConversationSummaryMemory(
+    # chat_memory=FileChatMessageHistory("messages.json"),
+    memory_key="messages", 
+    return_messages=True,
+    llm=chat,
 )
 
-test_prompt = PromptTemplate(
-    input_variables=["language", "code"],
-    template="Write a test for the following {language} code:\n{code}"
+
+
+prompt = ChatPromptTemplate(
+    input_variables=["content", "messages"],
+    messages=[
+        MessagesPlaceholder(variable_name="messages"),
+        HumanMessagePromptTemplate.from_template("{content}")
+    ]
 )
 
-code_chain = LLMChain(
-    llm=llm,
-    prompt=code_prompt,
-    output_key="code"
+chain = LLMChain(
+    llm=chat,
+    prompt=prompt,
+    memory=memory
 )
 
-test_chain = LLMChain(
-    llm=llm,
-    prompt=test_prompt,
-    output_key="test"
-)
-
-chain = SequentialChain(
-    chains=[code_chain, test_chain],
-    input_variables=["task", "language"],
-    output_variables=["test", "code"]
-)
-
-result = chain({
-    "language": args.language,
-    "task": args.task
-})
-print(result["code"])
-print(result["test"])
+while True: 
+    content = input(">> ")
+    result = chain({"content": content})
+    print(result["text"])
+    
